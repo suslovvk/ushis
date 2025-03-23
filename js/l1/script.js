75c7b877b40c236a57adb49e4434fb32616aed81
suslovvk <suslovvk@gmail.com>
2025-03-23 23:38:56 +0500

    save1

---
+++ js/l1/script.js
@@ -0,0 +1,27 @@
+'use strict';
+// alert  ('Hello, world!');
+
+// какой то комментарий
+// Второй комментарий
+// let hello = 'Hello, world!';
+// alert(hello);
+
+// let a = 2 ;
+// let b=5;
+// let sum =a+b;
+// alert("Сумма равна:"+sum);
+
+// const mVar= 9;
+// mVar=10;
+// alert(mVar);
+
+// let name = prompt ("Введите ваше имя");
+// alert("Привет,"+name+"!");
+function sum(a,b){
+    return a+b;
+}
+
+let age = 40;
+let num = 2;
+let name = "Вова";
+console.log(`Привет, меня зовут ${name} и мне ${sum(age,num)} лет.`);
