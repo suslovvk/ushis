64ccbf8570f64a20909d124179a0fc51a9e6a218
suslovvk <suslovvk@gmail.com>
2025-03-27 00:24:16 +0500

    save js2

---
+++ js/l2/script.js
@@ -0,0 +1,26 @@
+"use strict";
+// let client =confirm ("Вы постоянный клиент ? ");
+// let discount = client ? 30 : 10 ;
+// alert (` skidon ${discount} %`);
+
+/**
+ * Функция приветствует тебя или посылает в зависимости от твоего выбора ответа на вопрос.
+ * @returns {string} Строку приветствия вернет
+ */
+// function sayHello(){
+//     let hi = confirm("Привет , давай дружить? ");
+//     let answer = hi ? 'Хороший выбор': 'Ну и пошел ты нахер';
+//     // alert (answer);
+//     return answer;
+// }
+// alert(sayHello());
+
+/**
+ * 
+ * @param {number} a Первое слогаемое 
+ * @param {nunber} b  Второе слогаемое
+ * @returns Вернет сумму двух слогаемых )
+ */
+function sum (a ,b){
+    return a+b;
+}
